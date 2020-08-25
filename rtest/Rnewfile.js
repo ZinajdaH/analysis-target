48384c4289190b3d5811914b76ca42bd7cff3f5e
ZinajdaH <68381660+ZinajdaH@users.noreply.github.com>
2020-08-25 11:15:07 +0200

    Update and rename newfile.js to Rnewfile.js

---
+++ rtest/Rnewfile.js
@@ -0,0 +1,66 @@
+
+
+import type {
+  Instance,
+  TextInstance,
+  SuspenseInstance,
+  Container,
+  ChildSet,
+  UpdatePayload,
+} from './ReactFiberHostConfig';
+import type {Fiber} from './ReactInternalTypes';
+import type {FiberRoot} from './ReactInternalTypes';
+import type {Lanes} from './ReactFiberLane';
+import type {SuspenseState} from './ReactFiberSuspenseComponent.old';
+import type {UpdateQueue} from './ReactUpdateQueue.old';
+import type {FunctionComponentUpdateQueue} from './ReactFiberHooks.old';
+import type {Wakeable} from 'shared/ReactTypes';
+import type {ReactPriorityLevel} from './ReactInternalTypes';
+import type {OffscreenState} from './ReactFiberOffscreenComponent';
+
+import {unstable_wrap as Schedule_tracing_wrap} from 'scheduler/tracing';
+import {
+  enableSchedulerTracing,
+  enableProfilerTimer,
+  enableProfilerCommitHooks,
+  enableSuspenseServerRenderer,
+  enableFundamentalAPI,
+  enableSuspenseCallback,
+  enableScopeAPI,
+} from 'shared/ReactFeatureFlags';
+import {
+  FunctionComponent,
+  ForwardRef,
+  ClassComponent,
+  HostRoot,
+  HostComponent,
+  HostText,
+  HostPortal,
+  Profiler,
+  SuspenseComponent,
+  DehydratedFragment,
+  IncompleteClassComponent,
+  MemoComponent,
+  SimpleMemoComponent,
+  SuspenseListComponent,
+  FundamentalComponent,
+  ScopeComponent,
+  Block,
+  OffscreenComponent,
+  LegacyHiddenComponent,
+} from './ReactWorkTags';
+import {
+  invokeGuardedCallback,
+  hasCaughtError,
+  clearCaughtError,
+} from 'shared/ReactErrorUtils';
+import {
+  NoEffect,
+  ContentReset,
+  Placement,
+  Snapshot,
+  Update,
+} from './ReactSideEffectTags';
+import getComponentName from 'shared/getComponentName';
+import invariant from 'shared/invariant';
+
