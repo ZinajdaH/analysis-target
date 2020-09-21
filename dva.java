812b04016332b6c9033619ba99b442f35fadc811
ZinajdaH <68381660+ZinajdaH@users.noreply.github.com>
2020-09-21 13:04:07 +0200

    Update dva.java

--- dva.java
+++ dva.java
@@ -1,20 +1,4 @@
-import type {
-  Instance,
-  TextInstance,
-  SuspenseInstance,
-  Container,
-  ChildSet,
-  UpdatePayload,
-} from './ReactFiberHostConfig';
-import type {Fiber} from './ReactInternalTypes';
-import type {FiberRoot} from './ReactInternalTypes';
-import type {Lanes} from './ReactFiberLane';
-import type {SuspenseState} from './ReactFiberSuspenseComponent.old';
-import type {UpdateQueue} from './ReactUpdateQueue.old';
-import type {FunctionComponentUpdateQueue} from './ReactFiberHooks.old';
-import type {Wakeable} from 'shared/ReactTypes';
-import type {ReactPriorityLevel} from './ReactInternalTypes';
-import type {OffscreenState} from './ReactFiberOffscreenComponent';
+
 
 import {unstable_wrap as Schedule_tracing_wrap} from 'scheduler/tracing';
 import {
